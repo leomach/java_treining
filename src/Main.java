ec178adf909a9fc8c7ea9c917e79408a307f85b4
Leandro Machado <leandrovictoram@gmail.com>
2024-12-28 10:28:20 -0300

    Funções

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,34 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		// operações bit a bit (bitwise)
-		int mask = 0b100000;
-		int n = sc.nextInt();
+		// funções
+		System.out.println("Enter three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int higher = max(a, b, c);
 		
-		if ((n & mask) != 0) {
-			System.out.println("6th bit is true");
-		} else {
-			System.out.println("6th bit is false");
-		}
-			
+		showResult(higher);
+		
 		sc.close();
 	}
-
+	
+	public static int max(int x, int y, int z) {
+		int aux;
+		
+		if (x > y && x > z) {
+			aux = x;
+		} else if (y > z) {
+			aux = y;
+		} else {
+			aux = z;
+		}
+		
+		return aux;
+	}
+	
+	public static void showResult(int value) {
+		System.out.println("Higher = "+value);
+	}
 }
